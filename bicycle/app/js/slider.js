a0a0e18c6b3e4e17a3ec7f332f761b59301de6dd
Vitalik Surzhykyvych <surzhykyvychv@gmail.com>
2024-06-23 19:17:12 +0300

    bicycle full upload

---
+++ bicycle/app/js/slider.js
@@ -0,0 +1,16 @@
+if ($('.slider__body').length>0){
+  $('.slider__body').slick({
+    dots: true,
+    arrow: false,
+    accessibility: false,
+    slidesToShow: 1,
+    autoplaySpeed: 3000,
+    adaptiveHeight: true,
+    nextArrow: '<button type="button> class="slick-next></button>',
+    prevArrow: '<button type="button> class="slick-prev></button>',
+    responsive: [{
+    breakpoint: 768,
+    settings: {}
+    }]
+  });
+}
